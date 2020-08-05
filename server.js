409e43ade9334b04609e8d223b5d7bac491806b8
harsh <harsh710pat@gmail.com>
2020-08-06 01:25:23 +0530

    Hey Completed Cart and Auth Pages

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ const path = require('path')
 
 app.use(express.static('public'))
 
-app.get('/',function(req,res){
-    res.render("home")
-})
+
 
 //Set the template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine', 'ejs')
+
+app.get('/',function(req,res){
+    res.render("home")
+})
+
+app.get('/cart',(req,res)=> {
+    res.render('customers/cart')
+})
+
+app.get('/login',(req,res)=> {
+    res.render('auth/login')
+})
+
+app.get('/register',(req,res)=> {
+    res.render('auth/register')
+})
 
 app.listen(3000, () => {
     console.log(`Listening on port ${PORT}`)
